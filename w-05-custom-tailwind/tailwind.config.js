d792e74cf549331899a1674472863fb2714c6999
AbhishekChamp <abhishek96.r@gmail.com>
2024-10-26 20:03:54 +0530

    Week-05 Live class

---
+++ w-05-custom-tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            custom: "1440px", // Custom screen size
+        },
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
